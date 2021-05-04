c14ed2775320bbf551944a332a8d6917b6a4cc8b
Mafe Bracho <mafe.bracho@gmail.com>
2021-05-04 14:54:29 +0200

    initial commit

---
+++ BMI Calculator/Models/BMI.swift
@@ -0,0 +1,15 @@
+//
+//  BMI.swift
+//  BMI Calculator
+//
+//  Created by Tobi on 11.08.20.
+//  Copyright © 2020 Angela Yu. All rights reserved.
+//
+
+import UIKit
+
+struct BMI {
+    let value: Float
+    let advice: String
+    let color: UIColor
+}
